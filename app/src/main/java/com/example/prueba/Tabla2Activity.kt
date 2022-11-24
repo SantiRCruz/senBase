6e510a4d25baaf5871212ca070339de4085be16e
SantyRCruz <davidsantiagocruz02@gmail.com>
2022-11-24 14:09:00 -0500

    avance consumo especies y fase

--- app/src/main/java/com/example/prueba/Tabla2Activity.kt
+++ app/src/main/java/com/example/prueba/Tabla2Activity.kt
@@ -82,8 +82,8 @@ class Tabla2Activity : AppCompatActivity() {
             "0.0",
             "0.0",
             "",
-            "0.21",
-            "16.0",
+            "24",
+            "19.0",
             "",
             "",
             "0.0",
@@ -270,7 +270,7 @@ class Tabla2Activity : AppCompatActivity() {
             encontrarCantidadesPE()
         }
         binding.btnFosforo.setOnClickListener {
-            if (fosforoSeleccionada == null ) {
+            if (fosforoSeleccionada == null) {
                 Snackbar.make(binding.root, "Debe Seleccionar los datos", Snackbar.LENGTH_SHORT)
                     .show()
                 return@setOnClickListener
@@ -280,31 +280,38 @@ class Tabla2Activity : AppCompatActivity() {
     }
 
     private fun encontrarCantidadesFos() {
-        val resta = list[list.size-1].fosforo.toDouble() - list[list.size-2].fosforo.toDouble()
-        val cantidad = resta/(fosforoSeleccionada!!.fosf_disp.toDouble()/100)
-        Log.e("encontrarCantidadesFos: ",resta.toString() )
-        Log.e("encontrarCantidadesFos: ",fosforoSeleccionada!!.fosf_disp )
-        Log.e("encontrarCantidadesFos: ",cantidad.toString() )
-        list[3].cantidad = String.format("%.2f", cantidad)
+        Log.e("encontrarCantidadesFos: ", list[list.size - 1].fosforo.toDouble().toString())
+        Log.e("encontrarCantidadesFos: ", list[list.size - 2].fosforo.toDouble().toString())
+        val resta = list[list.size - 1].fosforo.toDouble() - list[list.size - 2].fosforo.toDouble()
+        val cantidad = resta / (fosforoSeleccionada!!.fosf_disp.toDouble() / 100)
+        Log.e("encontrarCantidadesFos: ", resta.toString())
+        Log.e("encontrarCantidadesFos: ", fosforoSeleccionada!!.fosf_disp)
+        Log.e("encontrarCantidadesFos: ", cantidad.toString())
+
         list[3].materia = fosforoSeleccionada!!.ingrediente
-        list[3].fibra = fosforoSeleccionada!!.fibra_cruda
-        list[3].energia = fosforoSeleccionada!!.e_m_ave
-        list[3].calcio = fosforoSeleccionada!!.calcio
-        list[3].fosforo = fosforoSeleccionada!!.fosf_disp
-        list[3].lisina = fosforoSeleccionada!!.lisina
-        list[3].metionina = fosforoSeleccionada!!.metionina
-        list[3].metCis = fosforoSeleccionada!!.met_cis
+        list[3].cantidad = String.format("%.2f", cantidad)
+        //////////////////
+        list[3].proteina = (cantidad * (fosforoSeleccionada!!.proteina.toDouble() / 100)).toString()
+        list[3].fibra =   (cantidad * (fosforoSeleccionada!!.fibra_cruda.toDouble() / 100)).toString()
+        list[3].energia =  (cantidad * (fosforoSeleccionada!!.e_m_ave.toDouble() / 100)).toString()
+        list[3].calcio =  (cantidad * (fosforoSeleccionada!!.calcio.toDouble() / 100)).toString()
+        list[3].fosforo =  (cantidad * (fosforoSeleccionada!!.fosf_disp.toDouble() / 100)).toString()
+        list[3].lisina =  (cantidad * (fosforoSeleccionada!!.lisina.toDouble() / 100)).toString()
+        list[3].metionina =   (cantidad * (fosforoSeleccionada!!.metionina.toDouble() / 100)).toString()
+        list[3].metCis =  (cantidad * (fosforoSeleccionada!!.met_cis.toDouble() / 100)).toString()
+        //////////////////
+        list[list.size-2].cantidad = (list[list.size-2].cantidad.toDouble()+list[3].cantidad.toDouble()).toString()
+        list[list.size-2].proteina = (list[list.size-2].proteina.toDouble()+list[3].proteina.toDouble()).toString()
+        list[list.size-2].fibra = (list[list.size-2].fibra.toDouble()+list[3].fibra.toDouble()).toString()
+        list[list.size-2].energia = (list[list.size-2].energia.toDouble()+list[3].energia.toDouble()).toString()
+        list[list.size-2].calcio = (list[list.size-2].calcio.toDouble()+list[3].calcio.toDouble()).toString()
+        list[list.size-2].fosforo = (list[list.size-2].fosforo.toDouble()+list[3].fosforo.toDouble()).toString()
+        list[list.size-2].lisina = (list[list.size-2].lisina.toDouble()+list[3].lisina.toDouble()).toString()
+        list[list.size-2].metionina = (list[list.size-2].metionina.toDouble()+list[3].metionina.toDouble()).toString()
+        list[list.size-2].metCis = (list[list.size-2].metCis.toDouble()+list[3].metCis.toDouble()).toString()
 
         setUpRecycler(list)
 
-    }
-
-    private fun encontrarCantidadesCal() {
-        val resta = list[list.size-1].calcio.toDouble() - list[list.size-2].calcio.toDouble()
-        val cantidad = resta/(calcioSeleccionada!!.calcio.toDouble()/100)
-        Log.e("encontrarCantidadesFos: ",resta.toString() )
-        Log.e("encontrarCantidadesFos: ",calcioSeleccionada!!.calcio )
-        Log.e("encontrarCantidadesFos: ",cantidad.toString() )
     }
 
     private fun añadirPrecio() {
@@ -333,27 +340,30 @@ class Tabla2Activity : AppCompatActivity() {
     }
 
     private fun encontrarCantidadesPE() {
-        val difEnergia: Double = if (energiaSeleccionada!!.proteina.toDouble() > Constants.faseActual.proteina.toDouble()) {
-            (energiaSeleccionada!!.proteina.toDouble() - Constants.faseActual.proteina.toDouble())
-        } else {
-            (Constants.faseActual.proteina.toDouble()-energiaSeleccionada!!.proteina.toDouble())
-        }
-        val difProteina: Double = if (proteinaSeleccionada!!.proteina.toDouble() > Constants.faseActual.proteina.toDouble()) {
-            (proteinaSeleccionada!!.proteina.toDouble() - Constants.faseActual.proteina.toDouble())
-        } else {
-            (Constants.faseActual.proteina.toDouble()-proteinaSeleccionada!!.proteina.toDouble())
-        }
+        val difEnergia: Double =
+            if (energiaSeleccionada!!.proteina.toDouble() > Constants.faseActual.proteina.toDouble()) {
+                (energiaSeleccionada!!.proteina.toDouble() - Constants.faseActual.proteina.toDouble())
+            } else {
+                (Constants.faseActual.proteina.toDouble() - energiaSeleccionada!!.proteina.toDouble())
+            }
+        val difProteina: Double =
+            if (proteinaSeleccionada!!.proteina.toDouble() > Constants.faseActual.proteina.toDouble()) {
+                (proteinaSeleccionada!!.proteina.toDouble() - Constants.faseActual.proteina.toDouble())
+            } else {
+                (Constants.faseActual.proteina.toDouble() - proteinaSeleccionada!!.proteina.toDouble())
+            }
 
         val sum = difEnergia + difProteina
 
-        energiaSeleccionada!!.cantidad = (difProteina * cantidadKg ) / sum
-        proteinaSeleccionada!!.cantidad = (difEnergia * cantidadKg ) / sum
+        energiaSeleccionada!!.cantidad = (difProteina * cantidadKg) / sum
+        proteinaSeleccionada!!.cantidad = (difEnergia * cantidadKg) / sum
         list[1].materia = proteinaSeleccionada!!.ingrediente
         list[1].cantidad = String.format("%.2f", proteinaSeleccionada!!.cantidad)
         list[2].materia = energiaSeleccionada!!.ingrediente
         list[2].cantidad = String.format("%.2f", energiaSeleccionada!!.cantidad)
 
-        list[list.size-2].cantidad = (list[1].cantidad.toDouble() + list[2].cantidad.toDouble()).toString()
+        list[list.size - 2].cantidad =
+            (list[1].cantidad.toDouble() + list[2].cantidad.toDouble()).toString()
         balanceProteina()
         balanceFibra()
         balanceEm()
@@ -379,68 +389,91 @@ class Tabla2Activity : AppCompatActivity() {
     }
 
     private fun balanceFibra() {
-        val aporteEnerigia = (energiaSeleccionada!!.cantidad * energiaSeleccionada!!.fibra_cruda.toDouble()) / baseCien
-        val aporteProteina = (proteinaSeleccionada!!.cantidad * proteinaSeleccionada!!.fibra_cruda.toDouble()) / baseCien
-        list[1].fibra = String.format("%.2f",aporteProteina)
-        list[2].fibra = String.format("%.2f",aporteEnerigia)
-        list[list.size-2].fibra = (list[1].fibra.toDouble() + list[2].fibra.toDouble()).toString()
+        val aporteEnerigia =
+            (energiaSeleccionada!!.cantidad * energiaSeleccionada!!.fibra_cruda.toDouble()) / baseCien
+        val aporteProteina =
+            (proteinaSeleccionada!!.cantidad * proteinaSeleccionada!!.fibra_cruda.toDouble()) / baseCien
+        list[1].fibra = String.format("%.2f", aporteProteina)
+        list[2].fibra = String.format("%.2f", aporteEnerigia)
+        list[list.size - 2].fibra = (list[1].fibra.toDouble() + list[2].fibra.toDouble()).toString()
     }
 
     private fun balanceProteina() {
-        val aporteProteinaEnerigia = (energiaSeleccionada!!.cantidad * energiaSeleccionada!!.proteina.toDouble()) / baseCien
-        val aporteProteinaProteina = (proteinaSeleccionada!!.cantidad * proteinaSeleccionada!!.proteina.toDouble()) / baseCien
-        Log.e("balanceProteina: ", "" )
-        list[1].proteina = String.format("%.2f",aporteProteinaProteina)
-        list[2].proteina = String.format("%.2f",aporteProteinaEnerigia)
-        list[list.size-2].proteina = (list[1].proteina.toDouble() + list[2].proteina.toDouble()).toString()
+        val aporteProteinaEnerigia =
+            (energiaSeleccionada!!.cantidad * energiaSeleccionada!!.proteina.toDouble()) / baseCien
+        val aporteProteinaProteina =
+            (proteinaSeleccionada!!.cantidad * proteinaSeleccionada!!.proteina.toDouble()) / baseCien
+        Log.e("balanceProteina: ", "")
+        list[1].proteina = String.format("%.2f", aporteProteinaProteina)
+        list[2].proteina = String.format("%.2f", aporteProteinaEnerigia)
+        list[list.size - 2].proteina =
+            (list[1].proteina.toDouble() + list[2].proteina.toDouble()).toString()
     }
 
     private fun balanceEm() {
-        val aporteEnerigia = (energiaSeleccionada!!.cantidad * energiaSeleccionada!!.e_m_ave.toDouble()) / baseCien
-        val aporteProteina = (proteinaSeleccionada!!.cantidad * proteinaSeleccionada!!.e_m_ave.toDouble()) / baseCien
-        list[1].energia = String.format("%.2f",aporteProteina)
-        list[2].energia = String.format("%.2f",aporteEnerigia)
-        list[list.size-2].energia = (list[1].energia.toDouble() + list[2].energia.toDouble()).toString()
+        val aporteEnerigia =
+            (energiaSeleccionada!!.cantidad * energiaSeleccionada!!.e_m_ave.toDouble()) / baseCien
+        val aporteProteina =
+            (proteinaSeleccionada!!.cantidad * proteinaSeleccionada!!.e_m_ave.toDouble()) / baseCien
+        list[1].energia = String.format("%.2f", aporteProteina)
+        list[2].energia = String.format("%.2f", aporteEnerigia)
+        list[list.size - 2].energia =
+            (list[1].energia.toDouble() + list[2].energia.toDouble()).toString()
     }
 
     private fun balanceCalcio() {
-        val aporteEnerigia = (energiaSeleccionada!!.cantidad * energiaSeleccionada!!.calcio.toDouble()) / baseCien
-        val aporteProteina = (proteinaSeleccionada!!.cantidad * proteinaSeleccionada!!.calcio.toDouble()) / baseCien
-        list[1].calcio = String.format("%.2f",aporteProteina)
-        list[2].calcio = String.format("%.2f",aporteEnerigia)
-        list[list.size-2].calcio = (list[1].calcio.toDouble() + list[2].calcio.toDouble()).toString()
+        val aporteEnerigia =
+            (energiaSeleccionada!!.cantidad * energiaSeleccionada!!.calcio.toDouble()) / baseCien
+        val aporteProteina =
+            (proteinaSeleccionada!!.cantidad * proteinaSeleccionada!!.calcio.toDouble()) / baseCien
+        list[1].calcio = String.format("%.2f", aporteProteina)
+        list[2].calcio = String.format("%.2f", aporteEnerigia)
+        list[list.size - 2].calcio =
+            (list[1].calcio.toDouble() + list[2].calcio.toDouble()).toString()
     }
 
     private fun balanceFosforo() {
-        val aporteEnerigia = (energiaSeleccionada!!.cantidad * energiaSeleccionada!!.fosf_disp.toDouble()) / baseCien
-        val aporteProteina = (proteinaSeleccionada!!.cantidad * proteinaSeleccionada!!.fosf_disp.toDouble()) / baseCien
-        list[1].fosforo = String.format("%.2f",aporteProteina)
-        list[2].fosforo = String.format("%.2f",aporteEnerigia)
-        list[list.size-2].fosforo = (list[1].fosforo.toDouble() + list[2].fosforo.toDouble()).toString()
+        val aporteEnerigia =
+            (energiaSeleccionada!!.cantidad * energiaSeleccionada!!.fosf_disp.toDouble()) / baseCien
+        val aporteProteina =
+            (proteinaSeleccionada!!.cantidad * proteinaSeleccionada!!.fosf_disp.toDouble()) / baseCien
+        list[1].fosforo = String.format("%.2f", aporteProteina)
+        list[2].fosforo = String.format("%.2f", aporteEnerigia)
+        list[list.size - 2].fosforo =
+            (list[1].fosforo.toDouble() + list[2].fosforo.toDouble()).toString()
     }
 
     private fun balanceLisina() {
-        val aporteEnerigia = (energiaSeleccionada!!.cantidad * energiaSeleccionada!!.lisina.toDouble()) / baseCien
-        val aporteProteina = (proteinaSeleccionada!!.cantidad * proteinaSeleccionada!!.lisina.toDouble()) / baseCien
-        list[1].lisina = String.format("%.2f",aporteProteina)
-        list[2].lisina = String.format("%.2f",aporteEnerigia)
-        list[list.size-2].lisina = (list[1].lisina.toDouble() + list[2].lisina.toDouble()).toString()
+        val aporteEnerigia =
+            (energiaSeleccionada!!.cantidad * energiaSeleccionada!!.lisina.toDouble()) / baseCien
+        val aporteProteina =
+            (proteinaSeleccionada!!.cantidad * proteinaSeleccionada!!.lisina.toDouble()) / baseCien
+        list[1].lisina = String.format("%.2f", aporteProteina)
+        list[2].lisina = String.format("%.2f", aporteEnerigia)
+        list[list.size - 2].lisina =
+            (list[1].lisina.toDouble() + list[2].lisina.toDouble()).toString()
     }
 
     private fun balanceMetionina() {
-        val aporteEnerigia = (energiaSeleccionada!!.cantidad * energiaSeleccionada!!.metionina.toDouble()) / baseCien
-        val aporteProteina = (proteinaSeleccionada!!.cantidad * proteinaSeleccionada!!.metionina.toDouble()) / baseCien
-        list[1].metionina = String.format("%.2f",aporteProteina)
-        list[2].metionina = String.format("%.2f",aporteEnerigia)
-        list[list.size-2].metionina =  String.format("%.2f",(list[1].metionina.toDouble() + list[2].metionina.toDouble()))
+        val aporteEnerigia =
+            (energiaSeleccionada!!.cantidad * energiaSeleccionada!!.metionina.toDouble()) / baseCien
+        val aporteProteina =
+            (proteinaSeleccionada!!.cantidad * proteinaSeleccionada!!.metionina.toDouble()) / baseCien
+        list[1].metionina = String.format("%.2f", aporteProteina)
+        list[2].metionina = String.format("%.2f", aporteEnerigia)
+        list[list.size - 2].metionina =
+            String.format("%.2f", (list[1].metionina.toDouble() + list[2].metionina.toDouble()))
     }
 
     private fun balanceMetCis() {
-        val aporteEnerigia = (energiaSeleccionada!!.cantidad * energiaSeleccionada!!.met_cis.toDouble()) / baseCien
-        val aporteProteina = (proteinaSeleccionada!!.cantidad * proteinaSeleccionada!!.met_cis.toDouble()) / baseCien
-        list[1].metCis = String.format("%.2f",aporteProteina)
-        list[2].metCis = String.format("%.2f",aporteEnerigia)
-        list[list.size-2].metCis = (list[1].metCis.toDouble() + list[2].metCis.toDouble()).toString()
+        val aporteEnerigia =
+            (energiaSeleccionada!!.cantidad * energiaSeleccionada!!.met_cis.toDouble()) / baseCien
+        val aporteProteina =
+            (proteinaSeleccionada!!.cantidad * proteinaSeleccionada!!.met_cis.toDouble()) / baseCien
+        list[1].metCis = String.format("%.2f", aporteProteina)
+        list[2].metCis = String.format("%.2f", aporteEnerigia)
+        list[list.size - 2].metCis =
+            (list[1].metCis.toDouble() + list[2].metCis.toDouble()).toString()
     }
 
     private fun setUpRecycler(list: MutableList<Table>) {
